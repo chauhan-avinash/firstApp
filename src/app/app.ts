581b96c90cd0b44e21b557dc1c9ac212b4964eb7
Avinash Chauhan <avinash.paperbyte@gmail.com>
2026-08-05 10:05:34 +0530

    test

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,4 +10,35 @@ import { Component, signal } from '@angular/core';
 export class App {
   protected readonly title = signal('firstApp');
   value = '0.00';
+  scale: number = 1;
+  step: number = 0.1;
+  minScale: number = 0.5;
+  maxScale: number = 3;
+
+  products = [
+    { name: 'BumTum', image: '/bumtum1.jfif' },
+    { name: 'Combo', image: '/image1.png' },
+    { name: 'BumTum', image: '/bumtum1.jfif' },
+    { name: 'Combo', image: '/image1.png' },
+    { name: 'BumTum', image: '/bumtum1.jfif' },
+    { name: 'Combo', image: '/image1.png' },
+    { name: 'BumTum', image: '/bumtum1.jfif' },
+    { name: 'Combo', image: '/image1.png' }
+  ];
+
+  zoomIn() {
+    if (this.scale < this.maxScale) {
+      this.scale += this.step;
+    }
+  }
+
+  zoomOut() {
+    if (this.scale > this.minScale) {
+      this.scale -= this.step;
+    }
+  }
+
+  resetZoom() {
+    this.scale = 1;
+  }
 }
